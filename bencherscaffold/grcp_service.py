2255d1f901c367c4a5fa2ee4b2920c952b2897fd
Mona Lisa <leonard.papenmeier@posteo.de>
2024-02-10 19:57:35 +0100

    Add docstrings and refactor GRCPService class
    The updated GRCPService class now includes comprehensive docstrings for clarity. Furthermore, the class was refactored to handle the setup and initiation of the server, reducing code redundancy previously experienced with the usage of SecondLevelBencherServicer classes.

--- bencherscaffold/grcp_service.py
+++ bencherscaffold/grcp_service.py
@@ -15,15 +15,33 @@ class GRCPService(SecondLevelBencherServicer):
             port: int = 50000,
             n_cores: int | None = None
     ):
+        """
+        Args:
+            port: The port number to use for the connection. If not provided, the default port is set to 50000.
+            n_cores: The number of CPU cores to use for parallel processing. If not provided, it defaults to None.
+
+        """
         self.port = port
         self.n_cores = n_cores or os.cpu_count()
 
     def serve(
-            self,
+            self
     ):
+        """
+        Serves the gRPC server and starts listening on the specified port.
+
+        Args:
+            self (GRCPService): The instance of the SecondLevelBencherServicer class.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
         logging.basicConfig()
         n_cores = self.n_cores
-        port = str(self.ports)
+        port = str(self.port)
         server = grpc.server(ThreadPoolExecutor(max_workers=n_cores))
         second_level_services_pb2_grpc.add_SecondLevelBencherServicer_to_server(self, server)
         server.add_insecure_port("[::]:" + port)
