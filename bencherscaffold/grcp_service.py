a5bff02d9af76a2e2e562fae5f955d3e14b11e7b
Mona Lisa <leonard.papenmeier@posteo.de>
2024-02-10 19:54:59 +0100

    Refactor code to use new GRCPService class
    The code was refactored to replace the SecondLevelBencherServicer classes with the new GRCPService class. The new class encapsulates the logic of setting up and starting the server, removing the need to replicate this code in multiple places.

--- bencherscaffold/grcp_service.py
+++ bencherscaffold/grcp_service.py
@@ -10,14 +10,20 @@ from bencherscaffold.second_level_services_pb2_grpc import SecondLevelBencherSer
 
 class GRCPService(SecondLevelBencherServicer):
 
-    def serve(
+    def __init__(
             self,
             port: int = 50000,
             n_cores: int | None = None
     ):
+        self.port = port
+        self.n_cores = n_cores or os.cpu_count()
+
+    def serve(
+            self,
+    ):
         logging.basicConfig()
-        n_cores = n_cores or os.cpu_count()
-        port = str(port)
+        n_cores = self.n_cores
+        port = str(self.ports)
         server = grpc.server(ThreadPoolExecutor(max_workers=n_cores))
         second_level_services_pb2_grpc.add_SecondLevelBencherServicer_to_server(self, server)
         server.add_insecure_port("[::]:" + port)
