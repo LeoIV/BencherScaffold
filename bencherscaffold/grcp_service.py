6516be5881e6171fbe5091a99a6e73eb37b12935
Leonard Papenmeier <leonard.papenmeier@gmail.com>
2024-02-20 16:24:50 +0100

    feat: Add GRPC service initialization and create_protos.sh script
    - Added initialization of GRPC service with default port and n_cores
    - Added create_protos.sh script for generating protobuf files.

--- bencherscaffold/grcp_service.py
+++ bencherscaffold/grcp_service.py
@@ -1,9 +1,8 @@
-import logging
-import os
 from concurrent.futures import ThreadPoolExecutor
-from typing import Optional
 
 import grpc
+import logging
+import os
 
 from bencherscaffold import second_level_services_pb2_grpc
 from bencherscaffold.second_level_services_pb2_grpc import SecondLevelBencherServicer
@@ -14,16 +13,16 @@ class GRCPService(SecondLevelBencherServicer):
     def __init__(
             self,
             port: int = 50000,
-            n_cores: Optional[int] = None
+            n_cores: int = os.cpu_count()
     ):
         """
         Args:
             port: The port number to use for the connection. If not provided, the default port is set to 50000.
-            n_cores: The number of CPU cores to use for parallel processing. If not provided, it defaults to None.
+            n_cores: The number of CPU cores to use for parallel processing. If not provided, it defaults to os.cpu_count().
 
         """
         self.port = port
-        self.n_cores = n_cores or os.cpu_count()
+        self.n_cores = n_cores
 
     def serve(
             self
